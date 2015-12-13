234cafcfdc44cae121b772670dc834c654e1d23d
Onur Satici <onursatici@gmail.com>
2015-12-13 22:59:42 +0200

    promisify requests

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 #!/usr/local/bin/node
 
-var request = require('request');
+var P = require('bluebird');
+var request = require('bluebird').promisifyAll(require('request'));
+
 var cookieJar = request.jar();
 
-request.get({url:'https://leetcode.com',jar: cookieJar},function(err,data){
-      if(!err){
-        var formData = {};
-        formData.login = process.env.LEETCODENAME;
-        formData.password = process.env.LEETCODEPASS;
-        //TODO: fix the horrible code below
-        formData.csrfmiddlewaretoken = cookieJar._jar.store.idx['leetcode.com']['/'].csrftoken.toString().split(';')[0].split('=')[1];
+function fillFormData(res){
+  var formData = {};
+  formData.login = process.env.LEETCODENAME;
+  formData.password = process.env.LEETCODEPASS;
+  //TODO: fix the horrible code below
+  formData.csrfmiddlewaretoken = cookieJar._jar.store.idx['leetcode.com']['/'].csrftoken.toString().split(';')[0].split('=')[1];
+  return new P(function(resolve,reject){
+    resolve(formData);
+  });
+}
+function authenticate(formData){
+  return request.postAsync({url:'https://leetcode.com/accounts/login',jar: cookieJar, form:formData});
+}
 
-        request.post({url:'https://leetcode.com/accounts/login/', jar: cookieJar, form:formData},function(err,data){
-            //TODO cannot login, solve why
-              if(!err){
-                request.get({url:'https://leetcode.com/problemset/algorithms',jar:cookieJar},function(err,data){
-                      if(!err){
-                      }
-                    });
-              }
-            });
-      }
-    });
+
+request.getAsync({url:'https://leetcode.com',jar:cookieJar})
+  .then(fillFormData)
+  .then(authenticate)
+  .then(console.log); //log response
