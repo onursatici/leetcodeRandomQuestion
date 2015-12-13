250cd1be9358f52863672620354c74c04c58f989
Onur Satici <onursatici@gmail.com>
2015-12-14 01:48:41 +0200

    pivot to check folders, temporarily

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 #!/usr/local/bin/node
 
 var P = require('bluebird');
-var request = require('bluebird').promisifyAll(require('request'));
+var request = P.promisifyAll(require('request'));
+var cheerio = P.promisifyAll(require('cheerio'));
+var $;
 
-var cookieJar = request.jar();
-
-function fillFormData(res){
-  var formData = {};
-  formData.login = process.env.LEETCODENAME;
-  formData.password = process.env.LEETCODEPASS;
-  //TODO: fix the horrible code below
-  formData.csrfmiddlewaretoken = cookieJar._jar.store.idx['leetcode.com']['/'].csrftoken.toString().split(';')[0].split('=')[1];
-  return new P(function(resolve,reject){
-    resolve(formData);
-  });
+function listQuestions() {
+  return request.getAsync('https://leetcode.com/problemset/algorithms');
 }
-function authenticate(formData){
-  return request.postAsync({url:'https://leetcode.com/accounts/login',jar: cookieJar, form:formData});
+function parseQuestions(res){
+  $ = cheerio.load(res.body);
+  console.log($('#problemList').html());
 }
 
+listQuestions()
+  .then(parseQuestions);
 
-request.getAsync({url:'https://leetcode.com',jar:cookieJar})
-  .then(fillFormData)
-  .then(authenticate)
-  .then(console.log); //log response
