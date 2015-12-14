a6b39b2fdd2eb18610a7e881a080ae080cfc44f0
Onur Satici <onursatici@gmail.com>
2015-12-14 02:19:10 +0200

    finish parsing html to get questions object

--- index.js
+++ index.js
@@ -3,16 +3,34 @@
 var P = require('bluebird');
 var request = P.promisifyAll(require('request'));
 var cheerio = P.promisifyAll(require('cheerio'));
-var $;
 
 function listQuestions() {
   return request.getAsync('https://leetcode.com/problemset/algorithms');
 }
 function parseQuestions(res){
-  $ = cheerio.load(res.body);
-  console.log($('#problemList').html());
+  var $ = cheerio.load(res.body);
+  var questions = {};
+  $('#problemList tr').each(function(){
+
+    var children = $(this).children();
+    var number = $(children[1]).text().trim();
+    var name = $(children[2]).text().trim();
+    var difficulty = $(children[4]).text().trim();
+
+    questions[name.replace(/ /g,'').toLowerCase()] = {
+      number: number,
+      difficulty: difficulty
+    };
+
+  });
+  return new P(function(resolve,reject){
+    resolve(questions);
+  });
 }
+//TODO add check to folder names to see which questions are already done
+//TODO promisify child process and create the folder structure for the selected question
 
 listQuestions()
-  .then(parseQuestions);
+  .then(parseQuestions)
+  .then(console.log);
 
