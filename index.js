e612417f1be2acfec44ee4cf28cd0b8012731e52
Onur Satici <onursatici@gmail.com>
2015-12-16 19:36:21 +0200

    done getting unsolved questions

--- index.js
+++ index.js
@@ -3,34 +3,71 @@
 var P = require('bluebird');
 var request = P.promisifyAll(require('request'));
 var cheerio = P.promisifyAll(require('cheerio'));
+var fs = P.promisifyAll(require('fs'));
+
+var expandHomeDir = require('expand-home-dir');
 
 function listQuestions() {
   return request.getAsync('https://leetcode.com/problemset/algorithms');
 }
-function parseQuestions(res){
+
+String.prototype.capitalizeFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+function processName(name) {
+  return name.split(' ').map(function(el) {
+    return el.capitalizeFirstLetter();
+  }).join('');
+}
+
+function parseQuestions(res) {
   var $ = cheerio.load(res.body);
   var questions = {};
-  $('#problemList tr').each(function(){
+  $('#problemList tr').each(function() {
 
     var children = $(this).children();
     var number = $(children[1]).text().trim();
     var name = $(children[2]).text().trim();
     var difficulty = $(children[4]).text().trim();
 
-    questions[name.replace(/ /g,'').toLowerCase()] = {
+    questions[processName(name)] = {
       number: number,
       difficulty: difficulty
     };
 
   });
-  return new P(function(resolve,reject){
-    resolve(questions);
-  });
+  return questions;
 }
-//TODO add check to folder names to see which questions are already done
-//TODO promisify child process and create the folder structure for the selected question
 
-listQuestions()
-  .then(parseQuestions)
+function readLeetCodedir() {
+  return fs.readdirAsync(expandHomeDir('~/Desktop/LeetCode/'))
+    .then(removeHiddenFiles);
+}
+
+function removeHiddenFiles(files) {
+  if (files) {
+    return files.filter(function(el) {
+      return el && el.charAt(0) !== '.';
+    });
+  }
+}
+
+function removeSolvedQuestions(data) {
+  leetCodeQuestions = data[0];
+  solvedQuestions = data[1];
+  for (var i = 0; i < solvedQuestions.length; i++) {
+    var q = solvedQuestions[i];
+    if (leetCodeQuestions[q]) {
+      delete leetCodeQuestions[q];
+    }
+  }
+  return leetCodeQuestions;
+}
+
+P.all([
+    listQuestions().then(parseQuestions),
+    readLeetCodedir().then(removeHiddenFiles)
+  ])
+  .then(removeSolvedQuestions)
   .then(console.log);
-
